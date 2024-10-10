8a3547ca8638ec53db0be95282b67812e113cc38
Anees Ullah <aneesullah@gmail.com>
2024-10-10 07:46:19 +0500

    GCD repleaced with Adder and gcd with adder

--- generators/chipyard/src/main/scala/example/Adder.scala
+++ generators/chipyard/src/main/scala/example/Adder.scala
@@ -40,7 +40,7 @@ class AdderIO(val w: Int) extends Bundle {
   val busy = Output(Bool())
 }
 
-class HLSGCDAccelIO(val w: Int) extends Bundle {
+class HLSAdderAccelIO(val w: Int) extends Bundle {
   val ap_clk = Input(Clock())
   val ap_rst = Input(Reset())
   val ap_start = Input(Bool())
@@ -52,33 +52,33 @@ class HLSGCDAccelIO(val w: Int) extends Bundle {
   val ap_return = Output(UInt(w.W))
 }
 
-class GCDTopIO extends Bundle {
-  val gcd_busy = Output(Bool())
+class AdderTopIO extends Bundle {
+  val adder_busy = Output(Bool())
 }
 
-trait HasGCDTopIO {
-  def io: GCDTopIO
+trait HasAdderTopIO {
+  def io: AdderTopIO
 }
 
-// DOC include start: GCD blackbox
-class GCDMMIOBlackBox(val w: Int) extends BlackBox(Map("WIDTH" -> IntParam(w))) with HasBlackBoxResource {
-  val io = IO(new GCDIO(w))
-  addResource("/vsrc/GCDMMIOBlackBox.v")
+// DOC include start: Adder blackbox
+class AdderMMIOBlackBox(val w: Int) extends BlackBox(Map("WIDTH" -> IntParam(w))) with HasBlackBoxResource {
+  val io = IO(new AdderIO(w))
+  addResource("/vsrc/AdderMMIOBlackBox.v")
 }
-// DOC include end: GCD blackbox
+// DOC include end: Adder blackbox
 
-// DOC include start: GCD chisel
-class GCDMMIOChiselModule(val w: Int) extends Module {
-  val io = IO(new GCDIO(w))
+// DOC include start: Adder chisel
+class AdderMMIOChiselModule(val w: Int) extends Module {
+  val io = IO(new AdderIO(w))
   val s_idle :: s_run :: s_done :: Nil = Enum(3)
 
   val state = RegInit(s_idle)
   val tmp   = Reg(UInt(w.W))
-  val gcd   = Reg(UInt(w.W))
+  val adder   = Reg(UInt(w.W))
 
   io.input_ready := state === s_idle
   io.output_valid := state === s_done
-  io.gcd := gcd
+  io.adder := adder
 
   when (state === s_idle && io.input_valid) {
     state := s_run
@@ -89,23 +89,23 @@ class GCDMMIOChiselModule(val w: Int) extends Module {
   }
 
   when (state === s_idle && io.input_valid) {
-    gcd := io.x
+    adder := io.x
     tmp := io.y
   } .elsewhen (state === s_run) {
-    when (gcd > tmp) {
-      gcd := gcd - tmp
+    when (adder > tmp) {
+      adder := adder - tmp
     } .otherwise {
-      tmp := tmp - gcd
+      tmp := tmp - adder
     }
   }
 
   io.busy := state =/= s_idle
 }
-// DOC include end: GCD chisel
+// DOC include end: Adder chisel
 
 // DOC include start: HLS blackbox
-class HLSGCDAccelBlackBox(val w: Int) extends BlackBox with HasBlackBoxPath {
-  val io = IO(new HLSGCDAccelIO(w))
+class HLSAdderAccelBlackBox(val w: Int) extends BlackBox with HasBlackBoxPath {
+  val io = IO(new HLSAdderAccelIO(w))
 
   val chipyardDir = System.getProperty("user.dir")
   val hlsDir = s"$chipyardDir/generators/chipyard"
@@ -115,31 +115,31 @@ class HLSGCDAccelBlackBox(val w: Int) extends BlackBox with HasBlackBoxPath {
   require (make.! == 0, "Failed to run HLS")
 
   // Add each vlog file
-  addPath(s"$hlsDir/src/main/resources/vsrc/HLSGCDAccelBlackBox.v")
-  addPath(s"$hlsDir/src/main/resources/vsrc/HLSGCDAccelBlackBox_flow_control_loop_pipe.v")
+  addPath(s"$hlsDir/src/main/resources/vsrc/HLSAdderAccelBlackBox.v")
+  addPath(s"$hlsDir/src/main/resources/vsrc/HLSAdderAccelBlackBox_flow_control_loop_pipe.v")
 }
 // DOC include end: HLS blackbox
 
-// DOC include start: GCD router
-class GCDTL(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
-  val device = new SimpleDevice("gcd", Seq("ucbbar,gcd")) 
+// DOC include start: Adder router
+class AdderTL(params: AdderParams, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
+  val device = new SimpleDevice("adder", Seq("ucbbar,adder")) 
   val node = TLRegisterNode(Seq(AddressSet(params.address, 4096-1)), device, "reg/control", beatBytes=beatBytes)
 
-  override lazy val module = new GCDImpl
-  class GCDImpl extends Impl with HasGCDTopIO {
-    val io = IO(new GCDTopIO)
+  override lazy val module = new AdderImpl
+  class AdderImpl extends Impl with HasAdderTopIO {
+    val io = IO(new AdderTopIO)
     withClockAndReset(clock, reset) {
       // How many clock cycles in a PWM cycle?
       val x = Reg(UInt(params.width.W))
       val y = Wire(new DecoupledIO(UInt(params.width.W)))
-      val gcd = Wire(new DecoupledIO(UInt(params.width.W)))
+      val adder = Wire(new DecoupledIO(UInt(params.width.W)))
       val status = Wire(UInt(2.W))
 
       val impl_io = if (params.useBlackBox) {
-        val impl = Module(new GCDMMIOBlackBox(params.width))
+        val impl = Module(new AdderMMIOBlackBox(params.width))
         impl.io
       } else {
-        val impl = Module(new GCDMMIOChiselModule(params.width))
+        val impl = Module(new AdderMMIOChiselModule(params.width))
         impl.io
       }
 
@@ -151,14 +151,14 @@ class GCDTL(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends C
       impl_io.input_valid := y.valid
       y.ready := impl_io.input_ready
 
-      gcd.bits := impl_io.gcd
-      gcd.valid := impl_io.output_valid
-      impl_io.output_ready := gcd.ready
+      adder.bits := impl_io.adder
+      adder.valid := impl_io.output_valid
+      impl_io.output_ready := adder.ready
 
       status := Cat(impl_io.input_ready, impl_io.output_valid)
-      io.gcd_busy := impl_io.busy
+      io.adder_busy := impl_io.busy
 
-// DOC include start: GCD instance regmap
+// DOC include start: Adder instance regmap
       node.regmap(
         0x00 -> Seq(
           RegField.r(2, status)), // a read-only register capturing current status
@@ -167,29 +167,29 @@ class GCDTL(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends C
         0x08 -> Seq(
           RegField.w(params.width, y)), // write-only, y.valid is set on write
         0x0C -> Seq(
-          RegField.r(params.width, gcd))) // read-only, gcd.ready is set on read
-// DOC include end: GCD instance regmap
+          RegField.r(params.width, adder))) // read-only, adder.ready is set on read
+// DOC include end: Adder instance regmap
     }
   }
 }
 
-class GCDAXI4(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
+class AdderAXI4(params: AdderParams, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
   val node = AXI4RegisterNode(AddressSet(params.address, 4096-1), beatBytes=beatBytes)
-  override lazy val module = new GCDImpl
-  class GCDImpl extends Impl with HasGCDTopIO {
-    val io = IO(new GCDTopIO)
+  override lazy val module = new AdderImpl
+  class AdderImpl extends Impl with HasAdderTopIO {
+    val io = IO(new AdderTopIO)
     withClockAndReset(clock, reset) {
       // How many clock cycles in a PWM cycle?
       val x = Reg(UInt(params.width.W))
       val y = Wire(new DecoupledIO(UInt(params.width.W)))
-      val gcd = Wire(new DecoupledIO(UInt(params.width.W)))
+      val adder = Wire(new DecoupledIO(UInt(params.width.W)))
       val status = Wire(UInt(2.W))
 
       val impl_io = if (params.useBlackBox) {
-        val impl = Module(new GCDMMIOBlackBox(params.width))
+        val impl = Module(new AdderMMIOBlackBox(params.width))
         impl.io
       } else {
-        val impl = Module(new GCDMMIOChiselModule(params.width))
+        val impl = Module(new AdderMMIOChiselModule(params.width))
         impl.io
       }
 
@@ -201,12 +201,12 @@ class GCDAXI4(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends
       impl_io.input_valid := y.valid
       y.ready := impl_io.input_ready
 
-      gcd.bits := impl_io.gcd
-      gcd.valid := impl_io.output_valid
-      impl_io.output_ready := gcd.ready
+      adder.bits := impl_io.adder
+      adder.valid := impl_io.output_valid
+      impl_io.output_ready := adder.ready
 
       status := Cat(impl_io.input_ready, impl_io.output_valid)
-      io.gcd_busy := impl_io.busy
+      io.adder_busy := impl_io.busy
 
       node.regmap(
         0x00 -> Seq(
@@ -216,28 +216,28 @@ class GCDAXI4(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends
         0x08 -> Seq(
           RegField.w(params.width, y)), // write-only, y.valid is set on write
         0x0C -> Seq(
-          RegField.r(params.width, gcd))) // read-only, gcd.ready is set on read
+          RegField.r(params.width, adder))) // read-only, adder.ready is set on read
     }
   }
 }
-// DOC include end: GCD router
+// DOC include end: Adder router
 
-class HLSGCDAccel(params: GCDParams, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
-  val device = new SimpleDevice("hlsgcdaccel", Seq("ucbbar,hlsgcdaccel")) 
+class HLSAdderAccel(params: AdderParams, beatBytes: Int)(implicit p: Parameters) extends ClockSinkDomain(ClockSinkParameters())(p) {
+  val device = new SimpleDevice("hlsadderaccel", Seq("ucbbar,hlsadderaccel")) 
   val node = TLRegisterNode(Seq(AddressSet(params.address, 4096-1)), device, "reg/control", beatBytes=beatBytes)
 
-  override lazy val module = new HLSGCDAccelImpl
-  class HLSGCDAccelImpl extends Impl with HasGCDTopIO {
-    val io = IO(new GCDTopIO)
+  override lazy val module = new HLSAdderAccelImpl
+  class HLSAdderAccelImpl extends Impl with HasAdderTopIO {
+    val io = IO(new AdderTopIO)
     withClockAndReset(clock, reset) {
       val x = Reg(UInt(params.width.W))
       val y = Wire(new DecoupledIO(UInt(params.width.W)))
       val y_reg = Reg(UInt(params.width.W))
-      val gcd = Wire(new DecoupledIO(UInt(params.width.W)))
-      val gcd_reg = Reg(UInt(params.width.W))
+      val adder = Wire(new DecoupledIO(UInt(params.width.W)))
+      val adder_reg = Reg(UInt(params.width.W))
       val status = Wire(UInt(2.W))
 
-      val impl = Module(new HLSGCDAccelBlackBox(params.width))
+      val impl = Module(new HLSAdderAccelBlackBox(params.width))
 
       impl.io.ap_clk := clock
       impl.io.ap_rst := reset
@@ -252,20 +252,20 @@ class HLSGCDAccel(params: GCDParams, beatBytes: Int)(implicit p: Parameters) ext
       } .elsewhen (state === s_busy && impl.io.ap_done) {
         state := s_idle
         result_valid := true.B
-        gcd_reg := impl.io.ap_return
+        adder_reg := impl.io.ap_return
       }
 
       impl.io.ap_start := state === s_busy
 
-      gcd.valid := result_valid
+      adder.valid := result_valid
       status := Cat(impl.io.ap_idle, result_valid)
       
       impl.io.x := x
       impl.io.y := y_reg
       y.ready := impl.io.ap_idle
-      gcd.bits := gcd_reg
+      adder.bits := adder_reg
 
-      io.gcd_busy := !impl.io.ap_idle
+      io.adder_busy := !impl.io.ap_idle
 
       node.regmap(
         0x00 -> Seq(
@@ -275,60 +275,60 @@ class HLSGCDAccel(params: GCDParams, beatBytes: Int)(implicit p: Parameters) ext
         0x08 -> Seq(
           RegField.w(params.width, y)), // write-only, y.valid is set on write
         0x0C -> Seq(
-          RegField.r(params.width, gcd))) // read-only, gcd.ready is set on read
+          RegField.r(params.width, adder))) // read-only, adder.ready is set on read
     }
   }
 }
 
-// DOC include start: GCD lazy trait
-trait CanHavePeripheryGCD { this: BaseSubsystem =>
-  private val portName = "gcd"
+// DOC include start: Adder lazy trait
+trait CanHavePeripheryAdder { this: BaseSubsystem =>
+  private val portName = "adder"
 
   private val pbus = locateTLBusWrapper(PBUS)
 
   // Only build if we are using the TL (nonAXI4) version
-  val gcd_busy = p(GCDKey) match {
+  val adder_busy = p(AdderKey) match {
     case Some(params) => {
-      val gcd = if (params.useAXI4) {
-        val gcd = LazyModule(new GCDAXI4(params, pbus.beatBytes)(p))
-        gcd.clockNode := pbus.fixedClockNode
+      val adder = if (params.useAXI4) {
+        val adder = LazyModule(new AdderAXI4(params, pbus.beatBytes)(p))
+        adder.clockNode := pbus.fixedClockNode
         pbus.coupleTo(portName) {
-          gcd.node :=
+          adder.node :=
           AXI4Buffer () :=
           TLToAXI4 () :=
           // toVariableWidthSlave doesn't use holdFirstDeny, which TLToAXI4() needsx
           TLFragmenter(pbus.beatBytes, pbus.blockBytes, holdFirstDeny = true) := _
         }
-        gcd
+        adder
       } else if (params.useHLS) {
-        val gcd = LazyModule(new HLSGCDAccel(params, pbus.beatBytes)(p))
-        gcd.clockNode := pbus.fixedClockNode
-        pbus.coupleTo(portName) { gcd.node := TLFragmenter(pbus.beatBytes, pbus.blockBytes) := _ }
-        gcd
+        val adder = LazyModule(new HLSAdderAccel(params, pbus.beatBytes)(p))
+        adder.clockNode := pbus.fixedClockNode
+        pbus.coupleTo(portName) { adder.node := TLFragmenter(pbus.beatBytes, pbus.blockBytes) := _ }
+        adder
       } else {
-        val gcd = LazyModule(new GCDTL(params, pbus.beatBytes)(p))
-        gcd.clockNode := pbus.fixedClockNode
-        pbus.coupleTo(portName) { gcd.node := TLFragmenter(pbus.beatBytes, pbus.blockBytes) := _ }
-        gcd
+        val adder = LazyModule(new AdderTL(params, pbus.beatBytes)(p))
+        adder.clockNode := pbus.fixedClockNode
+        pbus.coupleTo(portName) { adder.node := TLFragmenter(pbus.beatBytes, pbus.blockBytes) := _ }
+        adder
       }
-      val gcd_busy = InModuleBody {
-        val busy = IO(Output(Bool())).suggestName("gcd_busy")
-        busy := gcd.module.io.gcd_busy
+      val adder_busy = InModuleBody {
+        val busy = IO(Output(Bool())).suggestName("adder_busy")
+        busy := adder.module.io.adder_busy
         busy
       }
-      Some(gcd_busy)
+      Some(adder_busy)
     }
     case None => None
   }
 }
-// DOC include end: GCD lazy trait
+// DOC include end: Adder lazy trait
 
-// DOC include start: GCD config fragment
-class WithGCD(useAXI4: Boolean = false, useBlackBox: Boolean = false, useHLS: Boolean = false) extends Config((site, here, up) => {
-  case GCDKey => {
+// DOC include start: Adder config fragment
+class WithAdder(useAXI4: Boolean = false, useBlackBox: Boolean = false, useHLS: Boolean = false) extends Config((site, here, up) => {
+  case AdderKey => {
     // useHLS cannot be used with useAXI4 and useBlackBox
     assert(!useHLS || (useHLS && !useAXI4 && !useBlackBox)) 
-    Some(GCDParams(useAXI4 = useAXI4, useBlackBox = useBlackBox, useHLS = useHLS))
+    Some(AdderParams(useAXI4 = useAXI4, useBlackBox = useBlackBox, useHLS = useHLS))
   }
 })
-// DOC include end: GCD config fragment
+// DOC include end: Adder config fragment
